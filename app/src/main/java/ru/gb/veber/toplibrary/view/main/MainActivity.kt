784f990f1e150978e7031d5397456958d4dac72c
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-10-12 14:26:03 +0300

    Change metod showUser

--- app/src/main/java/ru/gb/veber/toplibrary/view/main/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/toplibrary/view/main/MainActivity.kt
@@ -20,60 +20,6 @@ class MainActivity : MvpAppCompatActivity(), MainView{
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
-
-
-        //      var db = App.instance.database.userDao()
-//
-////        Log.d("DBTEST", "it.toString()")
-//        db.queryForAllUsers().subscribeOn(Schedulers.io()).subscribe({
-//            Log.d("DBTEST", it.size.toString())
-//            it.forEach {
-//                if(it.id==4||it.id==3||it.id==1){
-//                    Log.d("DBTEST", it.toString())
-//                }
-//            }
-//        },{
-//            Log.d("DBTEST", "errror")
-//            Log.d("DBTEST", it.localizedMessage)
-//        })
-//
-//        db.queryForAllRepos().subscribeOn(Schedulers.io()).subscribe({
-//            Log.d("DBTEST", it.size.toString())
-//            it.sortedBy { it.userId }.forEach {
-//                Log.d("DBTEST", it.name + " "+it.userId)
-//            }
-//        }, {
-//            Log.d("DBTEST", "errror")
-//            Log.d("DBTEST", it.localizedMessage)
-//        })
-//
-//
-//        db.deleteAllRepos().subscribeOn(Schedulers.io()).subscribe({
-//            Log.d("DBTEST", "success")
-//
-//        }, {
-//            Log.d("DBTEST", "errror")
-//            Log.d("DBTEST", it.localizedMessage)
-//        })
-//
-//        db.insertAllRepos(listOf(RepoDBObject(2, 1, "2", 4), RepoDBObject(3, 1, "2", 5)))
-//            .subscribeOn(Schedulers.io()).subscribe({
-//                Log.d("DBTEST", "success")
-//            },{
-//                Log.d("DBTEST", "errror")
-//                Log.d("DBTEST", it.localizedMessage)
-//            })
-
-//       db.queryForTest().subscribeOn(Schedulers.io()).subscribe({
-//           Log.d("DBTEST", "Success")
-//       },{
-//          Log.d("DBTEST", "errror")
-//          Log.d("DBTEST", it.localizedMessage)
-//       })
-
     }
 
     override fun onResumeFragments() {
